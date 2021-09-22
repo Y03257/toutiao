690b75f454194c767cb22f74355f6ff9ec7ac221
Y03257 <3124818170@qq.com>
2021-09-22 20:54:31 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <div><i class="iconfont icon-fenxiang"></i></div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <router-view />
   </div>
 </template>
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 <style lang="less">
-
 </style>
